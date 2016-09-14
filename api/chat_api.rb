ad6fc2504d9de87a006c81b17a8da8168f9c2b85
Aaron Blythe <ablythe@hearst.com>
2016-09-14 11:27:15 -0500

    working tests where the parameters are sent in.

--- api/chat_api.rb
+++ api/chat_api.rb
@@ -18,24 +18,38 @@ class MyApp < Sinatra::Base
 
 
   # Handles a POST request for '/v1/attachment'
-  # 
+  # @see https://api.slack.com/docs/message-attachments Slack Documentation
+  # @see https://api.slack.com/docs/messages/builder
   #
-  # @param request [Request] the request object
+  # @param [Hash] params the parameters sent on the request
+  # @option params [String] :channel The Slack Channel ID (Name may work in some instances)
+  # @option params [String] :text The text that will be posted in the channel
+  # @option params [String] :as_user ('true') 
+  # @option params [Hash] attachments the information about the attachments
+  # @option attachments [String] :fallback
+  # @option attachments [String] :pretext 
+  # @option attachments [String] :title
+  # @option attachments [String] :title_link 
+  # @option attachments [String] :text
+  # @option attachments [String] :color ('#7CD197') defaults to green if not specified
   # @return [String] the resulting webpage
   post '/v1/attachment' do
+    raise 'missing channel' unless params[:channel]
+
     logger.info('attach was called')
     @client.chat_postMessage(
-      channel: '#test_room',
-      text: 'Hello World',
-      as_user: true,
+      channel: params[:channel],
+      text: params[:text],
+      # TODO test the falsiness here
+      as_user: params[:as_user] ? params[:as_user] : true,
       attachments: [
         {
-          fallback: "New ticket from Andrea Lee - Ticket #1943: Can't rest my password - https://groove.hq/path/to/ticket/1943",
-          pretext: "New ticket from Andrea Lee",
-          title: "Ticket #1943: Can't reset my password",
-          title_link: "https://groove.hq/path/to/ticket/1943",
-          text: "Help! I tried to reset my password but nothing happened!",
-          color: "#7CD197"
+          fallback: params[:attachments][:fallback],
+          pretext: params[:attachments][:pretext],
+          title: params[:attachments][:title],
+          title_link: params[:attachments][:title_link],
+          text: params[:attachments][:text],
+          color: params[:attachments][:color] ? params[:attachments][:color] : '#7CD197'
         }
       ].to_json
     )
@@ -45,22 +59,39 @@ class MyApp < Sinatra::Base
     { 'message' => 'yes, it worked' }.to_json
   end
 
+  # Handles a POST request for '/v1/speak'
+  # @see https://api.slack.com/docs/message-formatting Slack Documentation
+  # @see https://api.slack.com/docs/messages/builder
+  #
+  # @param [Hash] params the parameters sent on the request
+  # @option params [String] :channel The Slack Channel ID (Name may work in some instances)
+  # @option params [String] :text The text that will be posted in the channel, supports formatting
+  # @option params [String] :as_user ('true')
+  # @return [String] the resulting webpage
   post '/v1/emote' do
     logger.info('emote was called')
     # interestingly... this did not work with name of room, only ID
-    @client.chat_meMessage(channel: 'C27MR7Y03',
-                          text: 'dances a jig')
+    @client.chat_meMessage(channel: params[:channel],
+                          text: params[:text])
     logger.info('posted to room')
     status 200
     { 'message' => 'yes, it worked' }.to_json
   end
 
-
+  # Handles a POST request for '/v1/speak'
+  # @see https://api.slack.com/docs/message-formatting Slack Documentation
+  # @see https://api.slack.com/docs/messages/builder
+  #
+  # @param [Hash] params the parameters sent on the request
+  # @option params [String] :channel The Slack Channel ID (Name may work in some instances)
+  # @option params [String] :text The text that will be posted in the channel, supports formatting
+  # @option params [String] :as_user ('true')
+  # @return [String] the resulting webpage
   post '/v1/speak' do
     logger.info('speak was called')
     @client.chat_postMessage(channel: params[:channel],
-                            text: '*Hello* `World` ~everbodeeeee~',
-                            as_user: true)
+                             text: params[:text],
+                             as_user: params[:as_user] ? params[:as_user] : true)
     logger.info('posted to room')
     status 200
     { 'message' => 'yes, it worked' }.to_json
--- spec/chat_api_spec.rb
+++ spec/chat_api_spec.rb
@@ -4,21 +4,32 @@ Capybara.app = MyApp
 
 describe 'call the speak API', type: :feature do
   it 'returns' do
-    page.driver.post '/v1/speak', channel: '#test_room'
+    page.driver.post '/v1/speak', channel: '#test_room', text: '*Hello* `World` ~everbodeeeee~'
     expect(page).to have_content 'it worked'
   end
 end
 
 describe 'call the emote API', type: :feature do
   it 'returns' do
-    page.driver.post '/v1/emote'
+    page.driver.post '/v1/emote', channel: 'C27MR7Y03', text: 'dances a jig'
     expect(page).to have_content 'it worked'
   end
 end
 
 describe 'call the attach API', type: :feature do
   it 'returns' do
-    page.driver.post '/v1/attachment'
+    page.driver.post '/v1/attachment',
+      channel: '#test_room',
+      text: 'Hello World',
+      attachments:
+        {
+          fallback: "New ticket from Andrea Lee - Ticket #1943: Can't rest my password - https://groove.hq/path/to/ticket/1943",
+          pretext: "New ticket from Andrea Lee",
+          title: "Ticket #1943: Can't reset my password",
+          title_link: "https://groove.hq/path/to/ticket/1943",
+          text: "Help! I tried to reset my password but nothing happened!",
+          color: "#7CD197"
+        }
     expect(page).to have_content 'it worked'
   end
 end
